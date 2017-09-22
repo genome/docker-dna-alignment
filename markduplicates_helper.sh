1eba8a8cfc406b68b36733abd8b8f099ffe72a22
Eddie Belter <ebelter@gmail.com>
2017-09-22 17:30:26 -0500

    Add barcode tag to mark dups helper

--- markduplicates_helper.sh
+++ markduplicates_helper.sh
@@ -3,4 +3,9 @@
 set -o pipefail
 set -o errexit
 
-/usr/bin/java -Xmx16g -jar /opt/picard/picard.jar MarkDuplicates I=$1 O=/dev/stdout ASSUME_SORT_ORDER=queryname METRICS_FILE=mark_dups_metrics.txt QUIET=true COMPRESSION_LEVEL=0 VALIDATION_STRINGENCY=LENIENT | /usr/bin/sambamba sort -t $2 -m 18G -o $3 /dev/stdin
+declare MD_BARCODE_TAG
+if [ ! -z "${4}" ]; then
+    MD_BARCODE_TAG="BARCODE_TAG=${4}"
+fi
+
+/usr/bin/java -Xmx16g -jar /opt/picard/picard.jar MarkDuplicates I=$1 O=/dev/stdout ASSUME_SORT_ORDER=queryname METRICS_FILE=mark_dups_metrics.txt QUIET=true COMPRESSION_LEVEL=0 VALIDATION_STRINGENCY=LENIENT "${MD_BARCODE_TAG}" | /usr/bin/sambamba sort -t $2 -m 18G -o $3 /dev/stdin
