3c042bc630583e8e5d64c5daff233d2804f61048
Jason Walker <jwalker@genome.wustl.edu>
2017-09-22 11:23:54 -0500

    pipe Picard SamToFastq into BWA-MEM using interleaved FASTQ format

--- alignment_helper.sh
+++ alignment_helper.sh
@@ -3,4 +3,4 @@
 set -o pipefail
 set -o errexit
 
-/usr/local/bin/bwa mem -K 100000000 -t $5 -Y -R "$1" $2 $3 $4 | /usr/local/bin/samblaster -a --addMateTags | /opt/samtools/bin/samtools view -b -S /dev/stdin
+/usr/bin/java -Xmx4g -jar /opt/picard/picard.jar SamToFastq I=$1 INTERLEAVE=true INCLUDE_NON_PF_READS=true FASTQ=/dev/stdout | /usr/local/bin/bwa mem -K 100000000 -t $4 -Y -p -R "$2" $3 /dev/stdin | /usr/local/bin/samblaster -a --addMateTags | /opt/samtools/bin/samtools view -b -S /dev/stdin
