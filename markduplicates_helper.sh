9e1d76c76ca3eed370a53f1bde3adbda31915cd8
Thomas Mooney <tmooney@genome.wustl.edu>
2017-12-13 09:40:27 -0600

    Don't add an empty string param if no MD tag value is available.
    The empty param makes Picard crash!

--- markduplicates_helper.sh
+++ markduplicates_helper.sh
@@ -6,6 +6,7 @@ set -o errexit
 declare MD_BARCODE_TAG
 if [ ! -z "${4}" ]; then
     MD_BARCODE_TAG="BARCODE_TAG=${4}"
+    /usr/bin/java -Xmx16g -jar /opt/picard/picard.jar MarkDuplicates I=$1 O=/dev/stdout ASSUME_SORT_ORDER=queryname METRICS_FILE=mark_dups_metrics.txt QUIET=true COMPRESSION_LEVEL=0 VALIDATION_STRINGENCY=LENIENT "${MD_BARCODE_TAG}" | /usr/bin/sambamba sort -t $2 -m 18G -o $3 /dev/stdin
+else
+    /usr/bin/java -Xmx16g -jar /opt/picard/picard.jar MarkDuplicates I=$1 O=/dev/stdout ASSUME_SORT_ORDER=queryname METRICS_FILE=mark_dups_metrics.txt QUIET=true COMPRESSION_LEVEL=0 VALIDATION_STRINGENCY=LENIENT | /usr/bin/sambamba sort -t $2 -m 18G -o $3 /dev/stdin
 fi
-
-/usr/bin/java -Xmx16g -jar /opt/picard/picard.jar MarkDuplicates I=$1 O=/dev/stdout ASSUME_SORT_ORDER=queryname METRICS_FILE=mark_dups_metrics.txt QUIET=true COMPRESSION_LEVEL=0 VALIDATION_STRINGENCY=LENIENT "${MD_BARCODE_TAG}" | /usr/bin/sambamba sort -t $2 -m 18G -o $3 /dev/stdin
